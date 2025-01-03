d72ded0ae72710071631eb3429e65df0272e9c09
Jenah <fonsecasantosjean@gmail.com>
2025-01-03 17:29:40 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    
+    $('header button').click(function() {
+            $('form').slideDown()
+    })
+    
+    $('form').on('reset', function(){
+        $('form').slideUp()
+    })
+
+
+    $('form').on('submit', function(e) {
+        e.preventDefault()
+        const enderecoNovaImg = $('#endereco-nova-imagem').val()
+        const novoItem = $('<li style="display:none;"></li>')
+        $(`<img src="${enderecoNovaImg}"/>`).appendTo(novoItem)
+        $(`<div class="overlay-imagem-link">
+                <a href="${enderecoNovaImg}" target="_blank" tittle="Ver imagem em tamanho real>
+                Ver imagem em tamanho real
+                </a>
+            </div>"'`).appendTo(novoItem)
+        $(novoItem).appendTo('ul')
+        $(novoItem).fadeIn()
+        $('endereco-nova-imagem').val() = ''
+
+    })
+})
+
